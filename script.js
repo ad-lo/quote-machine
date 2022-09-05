b6666becddae18d1a9afd6850626de63e6c371e6
ad-lo <102575296+ad-lo@users.noreply.github.com>
2022-09-04 21:52:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 let apiQuotes = [];
 
-// Get quotes from API
-async function getQuotes () {
-  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-  try {
-    const response = await Fetch(apiUrl);
-    apiQuotes = await response.json();
-    console.log(apiQuotes);
-  } catch (error) {
-    alert (error)
-    // Catch Error Here
-  }
+// Show new quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = apiQuotes [Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
 }
 
+// Get quotes from API
+// async function getQuotes() {
+//   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//     console.log(apiQuotes);
+//   } catch (error) {
+    
+//     // Catch Error Here
+//   }
+// }
+
 // On load
-getQuotes();
+// getQuotes();
+newQuote();
